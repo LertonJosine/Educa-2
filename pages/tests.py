33f34dd8c79b17fa909e87ce983db455e90a6f05
Lerton-dev <lertonjosine@gmail.com>
2025-07-10 15:56:05 +0200

    estilização das paginas de redefinição e mudança de senha e da pagina perfile.

--- pages/tests.py
+++ pages/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase, SimpleTestCase
-from .views import HomePageView
+from .views import HomePageView, TrainersView, AboutPageView
 from django.urls import reverse, resolve
 
 
@@ -13,9 +13,41 @@ class HomePageTest(SimpleTestCase):
     
     def test_home_page_template(self):
         self.assertTemplateUsed(self.answer, 'index.html')
-        self.assertContains(self.answer, 'Pagina Inicial')
     
     def test_home_page_view(self):
         view = resolve(reverse('home'))
         self.assertEqual(view.func.__name__, HomePageView.as_view().__name__)
 
+
+class TrainersPageTest(SimpleTestCase):
+    def setUp(self):
+        url = reverse('trainers')
+        self.answer = self.client.get(url)
+    
+    def test_trainers_page_name(self):
+        self.assertEqual(self.answer.status_code, 200)
+    
+    def test_trainers_page_template(self):
+        self.assertTemplateUsed(self.answer, 'trainers.html')
+        self.assertContains(self.answer, 'Trainers')
+    
+    def test_trainers_page_view(self):
+        view = resolve(reverse('trainers'))
+        self.assertEqual(view.func.__name__, TrainersView.as_view().__name__)
+
+
+class AboutPageTest(SimpleTestCase):
+    def setUp(self):
+        url = reverse('about')
+        self.answer = self.client.get(url)
+    
+    def test_about_page_name(self):
+        self.assertEqual(self.answer.status_code, 200)
+    
+    def test_about_page_template(self):
+        self.assertTemplateUsed(self.answer, 'about.html')
+        self.assertContains(self.answer, 'About')
+    
+    def test_about_page_view(self):
+        view = resolve(reverse('about'))
+        self.assertEqual(view.func.__name__, AboutPageView.as_view().__name__)

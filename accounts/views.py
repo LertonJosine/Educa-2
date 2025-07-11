9a4266899e05d67d813f82fde8a905d738259122
Lerton-dev <lertonjosine@gmail.com>
2025-07-11 14:35:29 +0200

    aplicação de restrições na pagina de perfil

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from django.contrib.auth import get_user_model
 from django.views.generic import CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 class SigupView(CreateView):
@@ -10,7 +11,11 @@ class SigupView(CreateView):
     form_class = CustomUserCreationForm
     
 
-class ProfileView(UpdateView):
+class ProfileView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = get_user_model()
     template_name = 'registration/profile.html'
     form_class = CustomUserChangeForm
+    
+    def test_func(self):
+        user = self.request.user
+        return user == self.get_object()

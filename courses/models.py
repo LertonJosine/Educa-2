22f3a6fb7a0cb0079609719c256e116ec33b9199
Lerton-dev <lertonjosine@gmail.com>
2025-07-11 15:25:58 +0200

    criação do modelo de cursos

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=150)
+    trainer = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    cover = models.FileField(upload_to='course/covers')
+    sits = models.IntegerField()
+    resume = models.TextField()
+    
+    def __str__(self):
+        return self.name[:100]
+    
+    def get_absolute_url(self):
+        return reverse("home")
+    

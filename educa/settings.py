13ef6144e875b9dd7b049b2f236813301f04f0bc
Lerton-dev <lertonjosine@gmail.com>
2025-07-14 22:11:27 +0200

    ajuste no localhost

--- educa/settings.py
+++ educa/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG')
 
-ALLOWED_HOSTS = ['localhost', 'https://educa-2.onrender.com']
+ALLOWED_HOSTS = ['localhost', 'educa-2.onrender.com']
 
 
 # Application definition

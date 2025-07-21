f1ed29087f633b04bcb8ee20f05ee03649baf295
Lerton-dev <lertonjosine@gmail.com>
2025-07-21 09:20:11 +0200

    new upload

--- educa/urls.py
+++ educa/urls.py
@@ -10,4 +10,4 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
     path('courses/', include('courses.urls')),
-] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
